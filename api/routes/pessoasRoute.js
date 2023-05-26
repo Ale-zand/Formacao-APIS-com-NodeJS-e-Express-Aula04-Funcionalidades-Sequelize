963b2eea3a3a9ea8147c361eb8f761045b0ae31f
Ale-zand <ale_zand@hotmail.com>
2023-05-26 17:40:48 -0300

    Paranoid, add colunas, softdelete and restore, harddelete

---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,22 @@
+const { Router } = require('express')
+const PessoaController = require('../controllers/PessoaController')
+
+const router = Router()
+
+router
+  .get('/pessoas', PessoaController.pegaTodasAsPessoas)
+  .get('/pessoas/viewdeletado', PessoaController.consultaTodasAsPessoasApagadas)
+  .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+  .get('/pessoas/:id/viewdeletado', PessoaController.consultaPessoaApagada)
+  .post('/pessoas', PessoaController.criaPessoa)
+  .put('/pessoas/:id', PessoaController.atualizaPessoa)
+  .delete('/pessoas/:id', PessoaController.apagaPessoa)
+  .delete('/pessoas/:id/harddeletepessoa', PessoaController.hardDeletePessoa)
+  .get('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.pegaUmaMatricula)
+  .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+  .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
+  .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
+  .put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
+  .delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
+
+module.exports = router
