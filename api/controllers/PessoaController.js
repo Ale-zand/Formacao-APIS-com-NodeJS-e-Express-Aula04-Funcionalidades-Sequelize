a4e2e66c3193e695be9cc4fbccad6a63bba80192
Ale-zand <ale_zand@hotmail.com>
2023-05-30 16:25:28 -0300

    .transaction. Rollback com transactions e cancelando matriculas com usuarios inativos

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -239,6 +239,21 @@ class PessoaController {
     }
   }
 
+  static async cancelaPessoa(req, res) {
+    const { estudanteId } = req.params
+    try {
+      database.sequelize.transaction(async transacao => {
+        await database.Pessoas
+          .update({ ativo: false }, { where: { id: Number(estudanteId)}}, { transaction: transacao })
+        await database.Matriculas
+          .update( { status: 'cancelado'}, { where: { estudante_id: Number(estudanteId)}}, { transaction: transacao })
+        return res.status(200).json({ message: `As matrículas referentes ao estudante ${estudanteId} foram canceladas.`})
+      })
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
 }
 
 module.exports = PessoaController
